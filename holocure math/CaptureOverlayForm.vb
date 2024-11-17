c620172a5be0f5f323a08f68fdd1f73eecf89eb0
mondinalegrid <119553792+mondinalegrid@users.noreply.github.com>
2024-11-17 13:54:08 +0800

    Add project files.

---
+++ holocure math/CaptureOverlayForm.vb
@@ -0,0 +1,118 @@
+﻿Public Class CaptureOverlayForm
+    Inherits Form
+
+    Private captureRect As Rectangle
+    Private dragStart As Point
+    Private isDragging As Boolean = False
+    Private moveStep As Integer = 10
+    Friend WithEvents TextBox1 As TextBox
+    Private resizeStep As Integer = 10
+
+    Public Sub New()
+        ' Set up the form as a transparent overlay
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.BackColor = Color.LimeGreen ' Pick a color that won't conflict with the background
+        Me.TransparencyKey = Color.LimeGreen ' Make this color transparent
+        Me.Opacity = 0.5 ' Make the form semi-transparent
+        Me.TopMost = True ' Ensure the form is always on top
+        Me.ShowInTaskbar = False
+        Me.Bounds = Screen.PrimaryScreen.Bounds ' Start as full screen
+
+        ' Initialize the rectangle (set an initial size and location)
+        captureRect = New Rectangle(800, 280, 200, 60) ' Initial area to capture
+
+        ' Hook up the mouse events
+        AddHandler Me.MouseDown, AddressOf OverlayForm_MouseDown
+        AddHandler Me.MouseMove, AddressOf OverlayForm_MouseMove
+        AddHandler Me.MouseUp, AddressOf OverlayForm_MouseUp
+
+        ' Handle KeyDown for arrow key movement and resizing
+        Me.KeyPreview = True
+        AddHandler Me.KeyDown, AddressOf OverlayForm_KeyDown
+    End Sub
+
+    ' Mouse events (same as before)
+    Private Sub OverlayForm_MouseDown(sender As Object, e As MouseEventArgs)
+        If captureRect.Contains(e.Location) Then
+            dragStart = e.Location
+            isDragging = True
+        End If
+    End Sub
+
+    Private Sub OverlayForm_MouseMove(sender As Object, e As MouseEventArgs)
+        If isDragging Then
+            Dim width As Integer = e.X - dragStart.X
+            Dim height As Integer = e.Y - dragStart.Y
+            captureRect = New Rectangle(dragStart.X, dragStart.Y, width, height)
+            Me.Invalidate()
+        End If
+    End Sub
+
+    Private Sub OverlayForm_MouseUp(sender As Object, e As MouseEventArgs)
+        isDragging = False
+    End Sub
+
+    Private Sub OverlayForm_KeyDown(sender As Object, e As KeyEventArgs)
+        ' Handle arrow keys for movement
+        Select Case e.KeyCode
+            Case Keys.Up
+                captureRect.Y -= moveStep
+            Case Keys.Down
+                captureRect.Y += moveStep
+            Case Keys.Left
+                captureRect.X -= moveStep
+            Case Keys.Right
+                captureRect.X += moveStep
+            Case Keys.Oemplus ' '+' key
+                captureRect.Height += resizeStep
+            Case Keys.OemMinus ' '-' key
+                captureRect.Height -= resizeStep
+            Case Keys.OemOpenBrackets ' '[' key
+                captureRect.Width -= resizeStep
+            Case Keys.OemCloseBrackets ' ']' key
+                captureRect.Width += resizeStep
+        End Select
+
+        ' Redraw the overlay form
+        Me.Invalidate()
+    End Sub
+
+    ' Paint the capture area (same as before)
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        Using brush As New SolidBrush(Color.FromArgb(100, 255, 0, 0))
+            e.Graphics.FillRectangle(brush, captureRect)
+        End Using
+        MyBase.OnPaint(e)
+    End Sub
+
+    Public Function CaptureImage() As Bitmap
+        ' Capture the selected area into an in-memory Bitmap
+        Dim screenCapture As New Bitmap(captureRect.Width, captureRect.Height)
+        Using g As Graphics = Graphics.FromImage(screenCapture)
+            g.CopyFromScreen(captureRect.Left, captureRect.Top, 0, 0, captureRect.Size)
+        End Using
+
+        Return screenCapture
+    End Function
+
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(139, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'CaptureOverlayForm
+        '
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "CaptureOverlayForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+End Class
---
+++ holocure math/Form1.vb
@@ -0,0 +1,269 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.BitmapExtension
+Imports Emgu.CV.Structure
+
+Public Class Form1
+
+    Private overlayForm As CaptureOverlayForm
+    Private image As Mat
+    Private templates As New Dictionary(Of String, Mat)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.ScrollBars = ScrollBars.Vertical
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If overlayForm Is Nothing OrElse overlayForm.IsDisposed Then
+            overlayForm = New CaptureOverlayForm()
+            overlayForm.Show()
+        Else
+            overlayForm.BringToFront()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Capture the selected area
+        If overlayForm IsNot Nothing Then
+            Dim test = overlayForm.CaptureImage
+            Dim extractedText = TemplateMatching(test).Replace("-2", "2").Replace("=", "").Replace("?", "").Trim
+            Dim evaluatedResults As String = ProcessMathExpressions(extractedText)
+
+            TextBox1.Text = TextBox1.Text & Environment.NewLine &
+                            "Extracted Expression: " & extractedText & Environment.NewLine &
+                            "Evaluated Results: " & evaluatedResults
+            TextBox1.SelectionStart = TextBox1.Text.Length
+            TextBox1.ScrollToCaret()
+
+            TextBox2.Text = extractedText
+        End If
+    End Sub
+
+    Private Function TemplateMatching(bitmapImage As Bitmap) As String
+        ' Load the main image
+        image = ToMat(bitmapImage)
+
+        ' Convert image to grayscale
+        Dim grayImage As Mat = New Mat()
+        CvInvoke.CvtColor(image, grayImage, ColorConversion.Bgr2Gray)
+        CvInvoke.Threshold(image, image, 127, 255, ThresholdType.Binary)
+
+        Dim path_to_file = Application.StartupPath & "templates\"
+
+        templates.Clear()
+        ' Load the template images
+        templates.Add("0", CvInvoke.Imread($"{path_to_file}0.png", ImreadModes.Grayscale))
+        templates.Add("1", CvInvoke.Imread($"{path_to_file}1.png", ImreadModes.Grayscale))
+        templates.Add("2", CvInvoke.Imread($"{path_to_file}2.png", ImreadModes.Grayscale))
+        templates.Add("3", CvInvoke.Imread($"{path_to_file}3.png", ImreadModes.Grayscale))
+        templates.Add("4", CvInvoke.Imread($"{path_to_file}4.png", ImreadModes.Grayscale))
+        templates.Add("5", CvInvoke.Imread($"{path_to_file}5.png", ImreadModes.Grayscale))
+        templates.Add("6", CvInvoke.Imread($"{path_to_file}6.png", ImreadModes.Grayscale))
+        templates.Add("7", CvInvoke.Imread($"{path_to_file}7.png", ImreadModes.Grayscale))
+        templates.Add("8", CvInvoke.Imread($"{path_to_file}8.png", ImreadModes.Grayscale))
+        templates.Add("9", CvInvoke.Imread($"{path_to_file}9.png", ImreadModes.Grayscale))
+        templates.Add(")", CvInvoke.Imread($"{path_to_file}closedp.png", ImreadModes.Grayscale))
+        templates.Add("/", CvInvoke.Imread($"{path_to_file}divide.png", ImreadModes.Grayscale))
+        templates.Add("*", CvInvoke.Imread($"{path_to_file}multiply.png", ImreadModes.Grayscale))
+        templates.Add("(", CvInvoke.Imread($"{path_to_file}openp.png", ImreadModes.Grayscale))
+        templates.Add("+", CvInvoke.Imread($"{path_to_file}plus.png", ImreadModes.Grayscale))
+        templates.Add("?", CvInvoke.Imread($"{path_to_file}question.png", ImreadModes.Grayscale))
+        templates.Add("=", CvInvoke.Imread($"{path_to_file}equals.png", ImreadModes.Grayscale))
+        templates.Add("-", CvInvoke.Imread($"{path_to_file}minus.png", ImreadModes.Grayscale))
+
+        ' Perform template matching
+        Return ExtractExpressionInOrder(grayImage)
+    End Function
+
+    ' Extract expression in order based on the x-position of the match
+    Private Function ExtractExpressionInOrder(mainImage As Mat) As String
+        Dim extractedText As New List(Of String)()
+        Dim matchResults As New List(Of (String, Point))()
+
+        ' Perform template matching for each template
+        For Each templatePair In templates
+            Dim template = templatePair.Value
+            Dim result As New Mat()
+
+            ' Match the template in the main image
+            CvInvoke.MatchTemplate(mainImage, template, result, TemplateMatchingType.CcoeffNormed)
+
+            ' Iterate through all matches above a confidence threshold
+            Dim threshold As Double = 0.8 ' Adjust as needed
+            Dim minDistance As Integer = 10 ' Minimum distance to consider as non-duplicate
+            While True
+                Dim minVal As Double, maxVal As Double
+                Dim minLoc As Point, maxLoc As Point
+                CvInvoke.MinMaxLoc(result, minVal, maxVal, minLoc, maxLoc)
+
+                ' Check if the match is above the threshold
+                If maxVal >= threshold Then
+                    ' Check for duplicates
+                    Dim isDuplicate As Boolean = matchResults.Any(Function(c) Math.Abs(c.Item2.X - maxLoc.X) < minDistance AndAlso Math.Abs(c.Item2.Y - maxLoc.Y) < minDistance)
+
+                    If Not isDuplicate Then
+                        If templatePair.Key = "-" Then
+                            Dim matchedWidth As Integer = template.Width
+                            Dim matchedHeight As Integer = template.Height
+                            Dim aspectRatio As Double = matchedWidth / matchedHeight
+                            Dim toleranceWidth As Integer = template.Width * 0.2
+                            Dim toleranceHeight As Integer = template.Height * 0.2
+                            Dim minAspectRatio As Double = aspectRatio * 0.8
+                            Dim maxAspectRatio As Double = aspectRatio * 1.2
+
+                            If Math.Abs(template.Width - matchedWidth) < toleranceWidth AndAlso
+                               Math.Abs(template.Height - matchedHeight) < toleranceHeight AndAlso
+                               aspectRatio > minAspectRatio AndAlso aspectRatio < maxAspectRatio Then
+                                ' Ensure match is not near the bottom of a number
+                                If Not IsNearNumberBottom(maxLoc, mainImage) Then
+                                    matchResults.Add((templatePair.Key, maxLoc))
+                                End If
+                            End If
+                        Else
+                            matchResults.Add((templatePair.Key, maxLoc))
+                        End If
+                    End If
+
+                    ' Suppress the current match to find the next one
+                    CvInvoke.Rectangle(result, New Rectangle(maxLoc, template.Size), New MCvScalar(0), -1)
+                Else
+                    Exit While ' No more matches above the threshold
+                End If
+            End While
+        Next
+
+        ' Sort the match results by the x-coordinate (left to right)
+        matchResults.Sort(Function(a, b) a.Item2.X.CompareTo(b.Item2.X))
+
+        ' Combine adjacent matches based on proximity (group digits together)
+        Dim currentGroup As String = ""
+        Dim lastX As Integer = -1
+
+        For Each match In matchResults
+            Dim matchValue As String = match.Item1
+            Dim matchX As Integer = match.Item2.X
+
+            ' If the current match is far enough from the last match, treat it as a new group
+            If lastX = -1 OrElse matchX > lastX + 20 Then ' Adjust threshold (e.g., 20 pixels) as needed
+                ' Add the previous group to the result
+                If currentGroup <> "" Then
+                    extractedText.Add(currentGroup)
+                End If
+                currentGroup = matchValue ' Start a new group
+            Else
+                ' Otherwise, append the current match to the existing group
+                currentGroup &= matchValue
+            End If
+
+            ' Update lastX to be the current match's x-coordinate
+            lastX = matchX
+        Next
+
+        ' Add the last group to the result if necessary
+        If currentGroup <> "" Then
+            extractedText.Add(currentGroup)
+        End If
+
+        ' Join the extracted text to form the expression
+        Return String.Join(" ", extractedText)
+    End Function
+
+    Private Function IsNearNumberBottom(location As Point, mainImage As Mat) As Boolean
+        ' Set a tolerance for proximity to the bottom of detected numbers
+        Dim proximityTolerance As Integer = 5 ' Adjust based on the size of characters in the image
+
+        ' Iterate through possible number templates to find bounding boxes
+        Dim path_to_file = Application.StartupPath & "templates\"
+        Dim numberTemplates As List(Of String) = New List(Of String) From {$"{path_to_file}0.png", $"{path_to_file}1.png", $"{path_to_file}2.png", $"{path_to_file}3.png", $"{path_to_file}4.png", $"{path_to_file}5.png", $"{path_to_file}6.png", $"{path_to_file}7.png", $"{path_to_file}8.png", $"{path_to_file}9.png"}
+        For Each templatePath In numberTemplates
+            ' Load the number template
+            Dim template As Mat = CvInvoke.Imread(templatePath, ImreadModes.Grayscale)
+
+            ' Perform template matching
+            Dim result As Mat = New Mat()
+            CvInvoke.MatchTemplate(mainImage, template, result, TemplateMatchingType.CcorrNormed)
+
+            Dim threshold As Double = 0.8 ' Confidence threshold for matching numbers
+            While True
+                Dim minVal As Double, maxVal As Double
+                Dim minLoc As Point, maxLoc As Point
+                CvInvoke.MinMaxLoc(result, minVal, maxVal, minLoc, maxLoc)
+
+                ' Check if the match is above the threshold
+                If maxVal >= threshold Then
+                    ' Get the bounding box of the detected number
+                    Dim detectedRect As Rectangle = New Rectangle(maxLoc, template.Size)
+
+                    ' Check if the detected location is near the bottom of this number
+                    Dim bottomY As Integer = detectedRect.Bottom
+                    If Math.Abs(location.Y - bottomY) <= proximityTolerance AndAlso
+                     location.X >= detectedRect.Left AndAlso location.X <= detectedRect.Right Then
+                        Return True ' The location is near the bottom of a number
+                    End If
+
+                    If detectedRect.Contains(location) Then
+                        Return True ' The location is inside or on the number
+                    End If
+
+                    ' Suppress the current match to find the next one
+                    CvInvoke.Rectangle(result, detectedRect, New MCvScalar(0), -1)
+                Else
+                    Exit While ' No more matches above the threshold
+                End If
+            End While
+        Next
+
+        ' If no number regions are near the location, return false
+        Return False
+    End Function
+
+    ' Process and evaluate math expressions
+    Private Function ProcessMathExpressions(extractedText As String) As String
+        ' Split the extracted text into individual expressions (assuming each expression is on a new line)
+        Dim mathExpressions As String() = extractedText.Split(New String() {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+
+        ' Process each expression and evaluate
+        Dim results As New List(Of String)()
+
+        For Each expression In mathExpressions
+            ' Clean up the expression (remove extra spaces, replace 'x' with '*')
+            expression = expression.Trim().Replace("x", "*")
+
+            ' Evaluate the math expression
+            Try
+                Dim result As String = EvaluateMathExpression(expression)
+                results.Add($"{expression} = {result}")
+            Catch ex As Exception
+                results.Add($"Error evaluating expression '{expression}': {ex.Message}")
+            End Try
+        Next
+
+        ' Return the evaluated results as a concatenated string
+        Return String.Join(Environment.NewLine, results)
+    End Function
+
+    ' Helper function to evaluate a single math expression
+    Private Function EvaluateMathExpression(expression As String) As String
+        Try
+            ' Use DataTable.Compute to evaluate the expression
+            Dim result As Object = New DataTable().Compute(expression, Nothing)
+
+            ' Return the result as a string
+            Return result.ToString()
+        Catch ex As Exception
+            ' If there's an error in the expression, return the error message
+            Return $"Error: {ex.Message}"
+        End Try
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim customText = TextBox2.Text
+        Dim evaluatedResults As String = ProcessMathExpressions(customText)
+
+        TextBox1.Text = TextBox1.Text & Environment.NewLine &
+                        "Extracted Expression: " & customText & Environment.NewLine &
+                        "Evaluated Results: " & evaluatedResults
+        TextBox1.SelectionStart = TextBox1.Text.Length
+        TextBox1.ScrollToCaret()
+    End Sub
+End Class
